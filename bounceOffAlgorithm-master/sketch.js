6cc8ab4a187ca6b45797a6ed863005bb1111ae05
Shashwat-A <66421850+Shashwat-A@users.noreply.github.com>
2021-03-08 12:46:19 +0530

    Add files via upload

---
+++ bounceOffAlgorithm-master/sketch.js
@@ -0,0 +1,36 @@
+var wall ,movingRect;
+var sq;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  sq = createSprite(500,600,50,50)
+
+ // movingRect.velocityY = -5;
+ // fixedRect.velocityY = +5;
+}
+
+function draw() {
+  background("white");  
+
+  movingRect.x = mouseX;
+  movingRect.y = mouseY;
+  //bounceOff(movingRect,fixedRect);
+
+  if (isTouching(movingRect,sq)) {
+    movingRect.shapeColor = "red";
+    sq.shapeColor = "red";
+  } else {
+    movingRect.shapeColor = "green";
+    sq.shapeColor = "green";
+  }
+  
+  drawSprites();
+}
